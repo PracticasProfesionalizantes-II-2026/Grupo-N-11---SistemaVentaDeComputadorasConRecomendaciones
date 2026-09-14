b4167c5b1b502f66c5244b97379d3cf7eacce46e
GabrielGF-arg <gaby_hammer@live.com.ar>
2026-09-14 20:17:17 -0300

    .

--- CompumundoFront/obj/Debug/net10.0/CompumundoFront.AssemblyInfo.cs
+++ CompumundoFront/obj/Debug/net10.0/CompumundoFront.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompumundoFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ec58f760620be54e18dc16e60e83f2b5c6d2b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba40f0a23eb049758b1bc0d504ecdb4b91cb2a27")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompumundoFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompumundoFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
